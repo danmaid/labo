bebfc5d578808d3e1c1f0a72cbadda14afdb988e
danmaid <yamada@danmaid.com>
2022-01-08 13:20:03 +0900

    overlay

--- packages/ui/src/views/Home.vue
+++ packages/ui/src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="home" style="position: relative">
+    <Snow style="position: absolute; z-index: -1"></Snow>
+    <div class="contents">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineAsyncComponent, defineComponent } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    Snow: defineAsyncComponent(() => import('./Snow.vue')),
   },
 })
 </script>
+
+<style>
+.contents {
+  background-color: rgba(100, 100, 255, 0.5);
+  margin: 0 100px;
+}
+</style>
