d21105772f54398621126b96abfbff7a81f8c20a
danmaid <yamada@danmaid.com>
2022-02-03 08:56:27 +0900

    simple server

--- packages/api-node/src/simple.ts
+++ packages/api-node/src/simple.ts
@@ -1,4 +1,7 @@
 import { Server } from 'http'
+import { WebSocket, WebSocketServer } from 'ws'
+import core from './core'
+import { v4 as uuid } from 'uuid'
 
 export const server = new Server()
 server.on('listening', () => {
@@ -18,4 +21,24 @@ server.on('close', () => {
 })
 server.on('request', (req, res) => {
   console.debug('request')
+  const id = uuid()
+  const { httpVersion, method, url, headers } = req
+  core.emit({ request: id, http: httpVersion, method, url, headers })
+
+  let body: any
+  req.on('data', (chunk) => {
+    body += chunk
+  })
+  req.on('end', () => {
+    if (body) core.emit({ request: id, body })
+    res.writeHead(200).end()
+  })
+})
+
+const wss = new WebSocketServer({ server })
+core.on(async (event) => {
+  const message = JSON.stringify(event)
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) client.send(message)
+  })
 })
