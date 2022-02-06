d6350dc1a3e0c445976eaede92dc99f3d8f56c96
danmaid <yamada@danmaid.com>
2022-02-06 21:48:31 +0900

    ui

--- packages/ui-vue/src/App.vue
+++ packages/ui-vue/src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <h1>labo</h1>
   <SubmitMemo></SubmitMemo>
+  <EventReceiver></EventReceiver>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import SubmitMemo from './components/SubmitMemo.vue'
+import EventReceiver from './components/EventReceiver.vue'
 
 export default defineComponent({
-  components: { SubmitMemo },
-  mounted() {
-    const ws = new WebSocket('ws://localhost:6900')
-    ws.addEventListener('message', (ev) => {
-      console.log(ev.data)
-    })
-  },
+  components: { SubmitMemo, EventReceiver },
 })
 </script>
---
+++ packages/ui-vue/src/components/EventReceiver.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="event of events">{{ event }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      ws: undefined as WebSocket | undefined,
+      events: [] as any[],
+    }
+  },
+  mounted() {
+    const ws = new WebSocket('ws://localhost:6900')
+    ws.addEventListener('message', (ev) => this.events.push(JSON.parse(ev.data)))
+    this.ws = ws
+  },
+  beforeUnmount() {
+    this.ws?.close()
+  },
+})
+</script>
